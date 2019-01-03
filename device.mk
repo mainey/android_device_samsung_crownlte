ef92b67bff5247b09bd597285cf6b3fe5c6ec8bf
Michael Benedict <michaelbt@live.com>
2019-01-04 01:36:16 +1100

    crownlte: import oreo ramdisk (temp)
    Signed-off-by: Michael Benedict <michaelbt@live.com>

--- device.mk
+++ device.mk
@@ -6,5 +6,16 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 # Property overrides
 -include $(LOCAL_PATH)/system_prop.mk
 
+# Init for crownlte
+PRODUCT_PACKAGES += \
+    fstab.samsungexynos9810 \
+    init.samsung.rc \
+    init.samsungexynos9810.rc \
+    init.samsungexynos9810.usb.rc \
+    init.baseband.rc \
+    init.gps.rc \
+    init.wifi.rc \
+    ueventd.samsungexynos9810.rc
+
 # Inherit common device configuration
 $(call inherit-product, device/samsung/universal9810-common/universal9810-common.mk)
